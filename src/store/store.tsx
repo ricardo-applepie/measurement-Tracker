222167704c3ba2f37876f4626739ca9ef43b0f6f
ricardo-ventzke <fkondem@gmail.com>
2021-12-30 16:15:17 +0100

    added subscriber comment

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./combinedReducers";
-
 import { composeWithDevTools } from "redux-devtools-extension";
+
 // creating  the Redux store , and using composeWithDevTools for debugging  .
 export const store = createStore(reducers, composeWithDevTools());
+
+// saving redux state to localStorage .
+
 store.subscribe(() => {
   localStorage.setItem("reduxState", JSON.stringify(store.getState()));
 });
