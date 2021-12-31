f2539bc4f96bf22e929708d942b943c2d9e48164
ricardo-ventzke <fkondem@gmail.com>
2021-12-31 15:55:01 +0100

    updated redux measurements variable to the plural form

--- src/pages/homepage/Home.tsx
+++ src/pages/homepage/Home.tsx
@@ -62,7 +62,6 @@ class HomePage extends React.Component<HomeProps, homepageState> {
     return (
       <div className="homepage">
         {updating && <div className="spinner-border" role="status"></div>}
-
         <section className="row form-section hero">
           <h1 className="center">Measurement Track</h1>
           <div className="col-md-3">
