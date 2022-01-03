47f96b9413dcd0f76a0029b42df9567bd4b7912c
ricardo-ventzke <fkondem@gmail.com>
2022-01-03 04:24:53 +0100

    adding date object prototypes

---
+++ src/config/utils.js
@@ -0,0 +1,26 @@
+Date.prototype.monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+Date.prototype.getMonthName = function () {
+  return this.monthNames[this.getMonth()];
+};
+Date.prototype.getShortMonthName = function () {
+  return this.getMonthName().substr(0, 3);
+};
+
+// usage:
+var d = new Date();
+alert(d.getMonthName()); // "October"
+alert(d.getShortMonthName()); //
